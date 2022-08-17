ff7e16036576f66592ea95714cecc066829728ab
Marko Milosev <101294904+Arfalas@users.noreply.github.com>
2022-08-17 16:01:02 +0200

    Update ConspTheo.py

--- ConspTheo.py
+++ ConspTheo.py
@@ -1,7 +1,7 @@
 from encodings import utf_8
 
 
-txtfile=open("C:/Users/No Name/OneDrive/Desktop/Projects/Ljotic.txt", "r", encoding="utf_8")
+txtfile=open("input_text", "r", encoding="utf_8")
 datazero=txtfile.read()
 data=datazero.casefold()
 data1=data.replace(',', '')
@@ -10,14 +10,17 @@ data3=data2.replace(':', '')
 data4=data3.replace("'", '')
 data5=data4.replace('?', '')
 data6=data5.replace('!', '')
-print(data6)
+#print(data6)
 dataList=data6.split()
-print(dataList)
+#print(dataList)
 def remove_words(list1, stop_words):
     for word in list(list1):
         if word in stop_words:
             list1.remove(word)
     return list1
-stop_wordsList=['i', 'do', 'o', 'nas', 'to', 'a', 'kao', 'se', 'će', 'ako', 'je', 'da', 'bi', 'na', 'od', 'smo', 'po', 'li', 'u', 'ka', 'taj', 'sa', 's', 'onaj', 'za', 'mi', 'ko']
-print(remove_words(dataList, stop_wordsList))
-print(len(stop_wordsList))
+stop_word_str=open('stop_words.txt', 'r', encoding='utf_8')
+stop_word=stop_word_str.read()
+stop_wordList=stop_word.split()
+#print(stop_wordList)
+pure_txt=remove_words(dataList, stop_wordList)
+print(pure_txt)
